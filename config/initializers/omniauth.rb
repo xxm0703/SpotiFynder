a8fd707c74bd85eed5309b82fcbd450685681f57
Martin Jordanov <xxm0703@abv.bg>
2021-09-24 15:47:32 +0300

    [Fix] OmniAuth blocks GET requests

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 require 'rspotify/oauth'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
+  OmniAuth.config.allowed_request_methods = [:get]
   provider :spotify, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], scope: 'user-read-email user-top-read user-library-read'
 end
